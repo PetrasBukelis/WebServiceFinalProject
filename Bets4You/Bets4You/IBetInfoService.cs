c5f544eea475b1bbdff653ae9c5a88d4a4cbb43d
Vaidotas Gružas <vaidotas.gruzas@stud.viko.lt>
2019-06-01 23:52:26 +0300

    Programos griaučiai
    Sukurtas WCF REST servisas, ir metodas, kuris atvaizduoja visus statymus iš DB

---
+++ Bets4You/Bets4You/IBetInfoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Bets4You
+{
+    [ServiceContract]
+    public interface IBetInfoService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedResponse)]
+        string AllBets();
+    }
+}
